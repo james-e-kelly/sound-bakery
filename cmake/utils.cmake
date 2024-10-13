b303aeedceb00a056ce9d4446444fb675bbb578b
James Kelly <jameskelly21699@gmail.com>
2024-10-13 22:14:04 +0100

    Return Clang library fix

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -2,7 +2,7 @@ macro(fix_linux_clang)
     if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND UNIX AND NOT APPLE)
         # Clang on Linux needs to use its own standard library headers and not the system default (gcc)
         # https://stackoverflow.com/questions/76859275/error-compiling-a-cpp-containing-stdchrono-errorstatic-constexpr-unsigned-fra
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+        string(APPEND CMAKE_CXX_FLAGS " -stdlib=libc++")
     endif()
 endmacro()
 
