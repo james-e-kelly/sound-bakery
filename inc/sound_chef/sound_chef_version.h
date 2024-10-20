b1a5298e6bc11cc6b479f3648637b3319bae6754
James Kelly <jameskelly21699@gmail.com>
2024-10-20 11:02:06 +0100

    Tidy main CMake file

--- inc/sound_chef/sound_chef_version.h
+++ inc/sound_chef/sound_chef_version.h
@@ -7,8 +7,8 @@
 #define SC_VERSION_MAJOR       0
 #define SC_VERSION_MINOR       1
 #define SC_VERSION_PATCH       0
-#define SC_VERSION_COMMIT_ID   "36cfbc8e61c63d433b07331997d9bdc2fcd44def"
+#define SC_VERSION_COMMIT_ID   "972ece4643ac569fd8ae1cf10c37a27c22f1354a"
 #define SC_VERSION_STRING      "0.1.0"
-#define SC_VERSION_STRING_FULL "0.1.0+36cfbc8e61c63d433b07331997d9bdc2fcd44def"
+#define SC_VERSION_STRING_FULL "0.1.0+972ece4643ac569fd8ae1cf10c37a27c22f1354a"
 
 #endif // SC_VERSION_H
