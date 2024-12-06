0497bcbd231d4b615d086a51cadc03989cf01133
James Kelly <jameskelly21699@gmail.com>
2024-12-06 11:55:15 +0000

    Only include dirent.h on Windows

--- inc/sound_bakery/sound_bakery_version.h
+++ inc/sound_bakery/sound_bakery_version.h
@@ -7,8 +7,8 @@
 #define SBK_VERSION_MAJOR       0
 #define SBK_VERSION_MINOR       1
 #define SBK_VERSION_PATCH       0
-#define SBK_VERSION_COMMIT_ID   "43ca68a9ceccfac0eadeddc03ef6e9993e1e9a62"
+#define SBK_VERSION_COMMIT_ID   "4416d9788895f5bababf4ffd1b987fc66b3bdddd"
 #define SBK_VERSION_STRING      "0.1.0"
-#define SBK_VERSION_STRING_FULL "0.1.0+43ca68a9ceccfac0eadeddc03ef6e9993e1e9a62"
+#define SBK_VERSION_STRING_FULL "0.1.0+4416d9788895f5bababf4ffd1b987fc66b3bdddd"
 
 #endif // SBK_VERSION_H
