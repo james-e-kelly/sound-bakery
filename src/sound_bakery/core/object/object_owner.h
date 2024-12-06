2b5903b619db6ba48bafecd4caeea3d2ed59f03f
James Kelly <jameskelly21699@gmail.com>
2024-12-06 21:49:08 +0000

    Add [[nodiscard]] to object_owner

--- src/sound_bakery/core/object/object_owner.h
+++ src/sound_bakery/core/object/object_owner.h
@@ -12,27 +12,28 @@ namespace sbk::core
     class SB_CLASS object_owner
     {
     public:
-        auto create_runtime_object(const rttr::type& type) -> std::shared_ptr<object>;
-        auto load_object(YAML::Node& node) -> std::shared_ptr<object>;
+        [[nodiscard]] auto create_runtime_object(const rttr::type& type) -> std::shared_ptr<object>;
+        [[nodiscard]] auto load_object(YAML::Node& node) -> std::shared_ptr<object>;
 
         /**
          * @param addToDatabase to automatically track the object. If set to false, the user is responsible for adding
          * the object to the database.
          */
-        auto create_database_object(const rttr::type& type, bool addToDatabase = true) -> std::shared_ptr<database_object>;
+        [[nodiscard]] auto create_database_object(const rttr::type& type, bool addToDatabase = true)
+            -> std::shared_ptr<database_object>;
 
         template <typename T>
-        auto create_runtime_object() -> std::shared_ptr<T>;
+        [[nodiscard]] auto create_runtime_object() -> std::shared_ptr<T>;
 
         template <typename T>
-        auto create_database_object() -> std::shared_ptr<T>;
+        [[nodiscard]] auto create_database_object() -> std::shared_ptr<T>;
 
         auto remove_object(const std::shared_ptr<object>& object) -> void;
 
         auto destroy_all() -> void;
 
-        auto get_objects() -> std::vector<std::shared_ptr<object>>&;
-        auto get_objects() const -> const std::vector<std::shared_ptr<object>>&;
+        [[nodiscard]] auto get_objects() -> std::vector<std::shared_ptr<object>>&;
+        [[nodiscard]] auto get_objects() const -> const std::vector<std::shared_ptr<object>>&;
 
     private:
         std::vector<std::shared_ptr<object>> m_objects;
