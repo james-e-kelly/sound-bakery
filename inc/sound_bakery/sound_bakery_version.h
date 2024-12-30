4860777817f5f07533a840f48b189ff9edbd5b3f
James Kelly <jameskelly21699@gmail.com>
2024-12-30 22:13:29 +0000

    Update Version To 0.2.0

--- inc/sound_bakery/sound_bakery_version.h
+++ inc/sound_bakery/sound_bakery_version.h
@@ -5,10 +5,10 @@
 #define SBK_PRODUCT_DESCRIPTION "An open-source audio middleware tool with an authoring application and programmer's API"
 
 #define SBK_VERSION_MAJOR       0
-#define SBK_VERSION_MINOR       1
+#define SBK_VERSION_MINOR       2
 #define SBK_VERSION_PATCH       0
-#define SBK_VERSION_COMMIT_ID   "24cac9c868990fa65c5fc707d0f8dba4506f0d2b"
-#define SBK_VERSION_STRING      "0.1.0"
-#define SBK_VERSION_STRING_FULL "0.1.0+24cac9c868990fa65c5fc707d0f8dba4506f0d2b"
+#define SBK_VERSION_COMMIT_ID   "357570dfad954bb7550756c378d1c098f92c26c9"
+#define SBK_VERSION_STRING      "0.2.0"
+#define SBK_VERSION_STRING_FULL "0.2.0+357570dfad954bb7550756c378d1c098f92c26c9"
 
 #endif // SBK_VERSION_H
--- inc/sound_chef/sound_chef_version.h
+++ inc/sound_chef/sound_chef_version.h
@@ -5,10 +5,10 @@
 #define SC_PRODUCT_DESCRIPTION "An open-source audio engine for games"
 
 #define SC_VERSION_MAJOR       0
-#define SC_VERSION_MINOR       1
+#define SC_VERSION_MINOR       2
 #define SC_VERSION_PATCH       0
-#define SC_VERSION_COMMIT_ID   "24cac9c868990fa65c5fc707d0f8dba4506f0d2b"
-#define SC_VERSION_STRING      "0.1.0"
-#define SC_VERSION_STRING_FULL "0.1.0+24cac9c868990fa65c5fc707d0f8dba4506f0d2b"
+#define SC_VERSION_COMMIT_ID   "357570dfad954bb7550756c378d1c098f92c26c9"
+#define SC_VERSION_STRING      "0.2.0"
+#define SC_VERSION_STRING_FULL "0.2.0+357570dfad954bb7550756c378d1c098f92c26c9"
 
 #endif // SC_VERSION_H
