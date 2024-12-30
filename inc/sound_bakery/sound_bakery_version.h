ced3b3f503cfb822926c72ebfa61a2afa62b97d0
James Kelly <jameskelly21699@gmail.com>
2024-12-30 22:58:02 +0000

    Update version

--- inc/sound_bakery/sound_bakery_version.h
+++ inc/sound_bakery/sound_bakery_version.h
@@ -5,10 +5,10 @@
 #define SBK_PRODUCT_DESCRIPTION "An open-source audio middleware tool with an authoring application and programmer's API"
 
 #define SBK_VERSION_MAJOR       0
-#define SBK_VERSION_MINOR       1
+#define SBK_VERSION_MINOR       2
 #define SBK_VERSION_PATCH       0
-#define SBK_VERSION_COMMIT_ID   "27f07999405af3d43cae5df9efc9f25fdd453570"
-#define SBK_VERSION_STRING      "0.1.0"
-#define SBK_VERSION_STRING_FULL "0.1.0+27f07999405af3d43cae5df9efc9f25fdd453570"
+#define SBK_VERSION_COMMIT_ID   "f110cb6b3c99180837aab9b63b339bd3c7da3fe9"
+#define SBK_VERSION_STRING      "0.2.0"
+#define SBK_VERSION_STRING_FULL "0.2.0+f110cb6b3c99180837aab9b63b339bd3c7da3fe9"
 
 #endif // SBK_VERSION_H
