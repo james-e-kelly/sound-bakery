bf3fbd642463399c9af555f5d7b9d3e111bf7ea0
James Kelly <jameskelly21699@gmail.com>
2024-12-30 22:45:18 +0000

    Remove yaml-cpp include

--- src/sound_bakery/pch.h
+++ src/sound_bakery/pch.h
@@ -30,8 +30,6 @@
 #include <variant>
 #include <vector>
 
-#include "yaml-cpp/yaml.h"
-
 /**
  * @def Registers this type's get_parent classes (if any) and marks its private members visible to reflection.
  */
