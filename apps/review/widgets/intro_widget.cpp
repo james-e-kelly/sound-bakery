f1b3be00e7c4b709c7a90e119065ac708669fd80
James Kelly <jameskelly21699@gmail.com>
2025-06-26 20:31:12 +0100

    Set intro_widget to hide tab

--- apps/review/widgets/intro_widget.cpp
+++ apps/review/widgets/intro_widget.cpp
@@ -10,58 +10,58 @@
 auto intro_widget::start_implementation() -> void
 {
     m_createWorkspacePopup = add_child_widget<create_workspace_popup>(false);
+
+	ImGuiWindowClass windowClass;
+    windowClass.DockNodeFlagsOverrideSet = ImGuiDockNodeFlags_NoTabBar;
+
+    set_window_class(windowClass);
 }
 
-auto intro_widget::render_implementation() -> void
+auto intro_widget::render_window_implementation() -> void
 {
-	if (ImGui::Begin(get_widget_name().data()))
+	gluten::layout centerColumnLayout(gluten::layout::layout_type::top_to_bottom, gluten::element::anchor_preset::stretch_center);
+    centerColumnLayout.get_element_anchor().maxOffset.x = 800;
+    centerColumnLayout.get_element_anchor().minOffset.x = -400;
+    centerColumnLayout.set_element_scale(1.5f);
+
+	centerColumnLayout.render_window();
+
+	gluten::text welcomeText("Welcome to Sound Check!", ImVec2(0.5f, 0.5f), gluten::element::anchor_preset::center_middle);
+	gluten::text secondText("To get started, create or open a workspace.", ImVec2(0.5f, 0.5f), gluten::element::anchor_preset::center_middle);
+
+	welcomeText.set_element_scale(1.5f);
+
+	centerColumnLayout.render_layout_element_pixels_vertical(&welcomeText, 100.f);
+	centerColumnLayout.render_layout_element_pixels_vertical(&secondText, 50.f);
+
+	gluten::layout twoColumnLayout(gluten::layout::layout_type::left_to_right, gluten::element::anchor_preset::stretch_full);
+    twoColumnLayout.set_element_frame_padding();
+
+    centerColumnLayout.render_layout_element_remaining(&twoColumnLayout);
+
+	gluten::button openButton("Open...", false, gluten::element::anchor_preset::center_top);
+	gluten::button createButton("Create...", false, gluten::element::anchor_preset::center_top);
+
+	openButton.set_element_alignment(ImVec2(0.5f, 0.f));
+	createButton.set_element_alignment(ImVec2(0.5f, 0.f));
+
+	if (twoColumnLayout.render_layout_element_percent_horizontal(&openButton, 0.5f))
 	{
-		gluten::layout centerColumnLayout(gluten::layout::layout_type::top_to_bottom, gluten::element::anchor_preset::stretch_center);
-        centerColumnLayout.get_element_anchor().maxOffset.x = 800;
-        centerColumnLayout.get_element_anchor().minOffset.x = -400;
-        centerColumnLayout.set_element_scale(1.5f);
+        const std::filesystem::path workspaceFile = gluten::app::open_select_file_dialog("workspace", "workspace");
 
-		centerColumnLayout.render_window();
-
-		gluten::text welcomeText("Welcome to Sound Check!", ImVec2(0.5f, 0.5f), gluten::element::anchor_preset::center_middle);
-		gluten::text secondText("To get started, create or open a workspace.", ImVec2(0.5f, 0.5f), gluten::element::anchor_preset::center_middle);
-
-		welcomeText.set_element_scale(1.5f);
-
-		centerColumnLayout.render_layout_element_pixels_vertical(&welcomeText, 100.f);
-		centerColumnLayout.render_layout_element_pixels_vertical(&secondText, 50.f);
-
-		gluten::layout twoColumnLayout(gluten::layout::layout_type::left_to_right, gluten::element::anchor_preset::stretch_full);
-        twoColumnLayout.set_element_frame_padding();
-
-        centerColumnLayout.render_layout_element_remaining(&twoColumnLayout);
-
-		gluten::button openButton("Open...", false, gluten::element::anchor_preset::center_top);
-		gluten::button createButton("Create...", false, gluten::element::anchor_preset::center_top);
-
-		openButton.set_element_alignment(ImVec2(0.5f, 0.f));
-		createButton.set_element_alignment(ImVec2(0.5f, 0.f));
-
-		if (twoColumnLayout.render_layout_element_percent_horizontal(&openButton, 0.5f))
+		if (std::shared_ptr<workspace_manager> workspaceManager = get_app()->get_manager_by_class<workspace_manager>())
 		{
-            const std::filesystem::path workspaceFile = gluten::app::open_select_file_dialog("workspace", "workspace");
-
-			if (std::shared_ptr<workspace_manager> workspaceManager = get_app()->get_manager_by_class<workspace_manager>())
-			{
-                workspaceManager->open_workspace(workspaceFile);
-			}
-		}
-
-		if (twoColumnLayout.render_layout_element_percent_horizontal(&createButton, 0.5f))
-		{
-			if (m_createWorkspacePopup)
-			{
-                m_createWorkspacePopup->open_popup();
-			}
+            workspaceManager->open_workspace(workspaceFile);
 		}
 	}
 
-	ImGui::End();
+	if (twoColumnLayout.render_layout_element_percent_horizontal(&createButton, 0.5f))
+	{
+		if (m_createWorkspacePopup)
+		{
+            m_createWorkspacePopup->open_popup();
+		}
+	}
 }
 
 auto create_workspace_popup::render_popup() -> void
--- apps/review/widgets/intro_widget.h
+++ apps/review/widgets/intro_widget.h
@@ -2,6 +2,7 @@
 
 #include "pch.h"
 
+#include "gluten/widgets/window_widget.h"
 #include "gluten/widgets/popup_widget.h"
 
 class create_workspace_popup : public gluten::popup_widget
@@ -13,13 +14,13 @@ private:
     virtual auto render_popup() -> void override;
 };
 
-class intro_widget : public gluten::widget
+class intro_widget : public gluten::window_widget
 {
-    WIDGET_CONSTRUCT(intro_widget, "Setup")
+    WIDGET_CONSTRUCT_PARENT(intro_widget, "Setup", gluten::window_widget)
 
 protected:
     virtual auto start_implementation() -> void override;
-    virtual auto render_implementation() -> void override;
+    virtual auto render_window_implementation() -> void override;
 
 private:
     std::shared_ptr<create_workspace_popup> m_createWorkspacePopup;
