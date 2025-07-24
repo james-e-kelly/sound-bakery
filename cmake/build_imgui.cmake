34d7ac3b94441f48f020f26fdfffe57f4319bc4b
James Kelly <jameskelly21699@gmail.com>
2025-07-24 21:15:17 +0100

    Add ImGuizmo, ImGuiTextSelect, and utf8cpp

--- cmake/build_imgui.cmake
+++ cmake/build_imgui.cmake
@@ -4,6 +4,22 @@ FetchContent_MakeAvailable(imgui)
 message(STATUS "Fetching implot")
 FetchContent_MakeAvailable(implot)
 
+CPMAddPackage(
+  NAME imguizmo
+  GITHUB_REPOSITORY CedricGuillemet/ImGuizmo
+  GIT_TAG master
+  EXCLUDE_FROM_ALL
+)
+
+CPMAddPackage(
+  NAME imguitextselect
+  GITHUB_REPOSITORY AidanSun05/ImGuiTextSelect
+  GIT_TAG main
+  EXCLUDE_FROM_ALL
+)
+
+CPMAddPackage("gh:nemtrif/utfcpp@4.0.6")
+
 add_library(imgui STATIC
   ${imgui_SOURCE_DIR}/imgui.cpp
   ${imgui_SOURCE_DIR}/imgui_demo.cpp
@@ -18,6 +34,13 @@ add_library(imgui STATIC
   ${implot_SOURCE_DIR}/implot.cpp
   ${implot_SOURCE_DIR}/implot_items.cpp
   ${implot_SOURCE_DIR}/implot_demo.cpp
+  ${imguizmo_SOURCE_DIR}/GraphEditor.cpp
+  ${imguizmo_SOURCE_DIR}/ImCurveEdit.cpp
+  ${imguizmo_SOURCE_DIR}/ImGradient.cpp
+  ${imguizmo_SOURCE_DIR}/ImGuizmo.cpp
+  ${imguizmo_SOURCE_DIR}/ImSequencer.cpp
+  ${imguitextselect_SOURCE_DIR}/textselect.cpp
+  ${imguitextselect_SOURCE_DIR}/textselect.hpp
 )
 
 c_17(imgui)
@@ -29,6 +52,8 @@ target_include_directories(imgui
     ${imgui_SOURCE_DIR}/backends
     ${imgui_SOURCE_DIR}/misc/cpp
     ${implot_SOURCE_DIR}
+    ${imguizmo_SOURCE_DIR}
+    ${imguitextselect_SOURCE_DIR}
 )
 
 find_package(OpenGL REQUIRED)
@@ -40,4 +65,5 @@ FetchContent_MakeAvailable(glfw)
 target_link_libraries(imgui PUBLIC
   glfw
   OpenGL::GL
+  utf8cpp
 )
