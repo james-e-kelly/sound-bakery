f1e5d345cc3827edc8791dd3e30028bce435abb0
James Kelly <jameskelly21699@gmail.com>
2025-07-31 15:39:33 +0100

    Quick hack to make images render on top without showing in popups

--- src/gluten/elements/image.cpp
+++ src/gluten/elements/image.cpp
@@ -37,11 +37,11 @@ namespace gluten
 
     bool image::render_element(const ImRect& elementRect)
     {
-        if (m_openGlId != 0 && m_width > 0 && m_height > 0)
+        if (m_openGlId != 0 && m_width > 0 && m_height > 0 && !ImGui::IsPopupOpen(nullptr, ImGuiPopupFlags_AnyPopup))
         {
             m_background.render(elementRect);
 
-            if (ImDrawList* const drawList = ImGui::GetWindowDrawList())
+            if (ImDrawList* const drawList = ImGui::GetForegroundDrawList())
             {
                 const ImVec2 elementRectSize = elementRect.GetSize();
 
