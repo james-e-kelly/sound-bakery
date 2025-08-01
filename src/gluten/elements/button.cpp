c9eca785eb155cd14801bf5a99143bb5750c1598
James Kelly <jameskelly21699@gmail.com>
2025-08-01 10:42:50 +0100

    Button colors

--- src/gluten/elements/button.cpp
+++ src/gluten/elements/button.cpp
@@ -1,6 +1,7 @@
 #include "button.h"
 
 #include "gluten/theme/theme.h"
+#include "gluten/utils/imgui_util_structures.h"
 #include "imgui_internal.h"
 
 gluten::button::button(const char* name, bool invisible, const anchor_preset& anchorPreset)
@@ -10,6 +11,11 @@ gluten::button::button(const char* name, bool invisible, const anchor_preset& an
 
 bool gluten::button::render_element(const ImRect& elementRect)
 {
+    gluten::imgui::scoped_color_stack buttonColors(
+        ImGuiCol_Button, m_activeColor.has_value() && m_active ? m_activeColor.value() : ImGui::GetColorU32(ImGuiCol_Button),
+        ImGuiCol_ButtonActive, m_activeColor.has_value() && m_active ? m_activeColor.value() : ImGui::GetColorU32(ImGuiCol_Button),
+        ImGuiCol_ButtonHovered, m_hoverColor.has_value() ? m_hoverColor.value() : ImGui::GetColorU32(ImGuiCol_ButtonHovered));
+
     const ImVec2 size = elementRect.GetSize();
 
     if (size.x < 0.1f || size.y < 0.1f)
