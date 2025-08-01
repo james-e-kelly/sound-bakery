b6c9bf83aed6226f01362184a06cc5699fc96802
James Kelly <jameskelly21699@gmail.com>
2025-08-01 17:00:14 +0100

    Add loading spinner element to gluten

---
+++ src/gluten/elements/loading_spinner.cpp
@@ -0,0 +1,12 @@
+#include "loading_spinner.h"
+
+#include "gluten/theme/carbon_theme_g100.h"
+
+#include "imspinner.h"
+
+auto gluten::loading_spinner::render_element(const ImRect& elementBox) -> bool
+{
+    ImSpinner::SpinnerAngEclipse("##Loading", ImGui::GetFontSize() / 2.0f, 2.0f, gluten::theme::white, 8.0f);
+
+    return false;
+}
---
+++ src/gluten/elements/loading_spinner.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "gluten/pch.h"
+
+#include "gluten/elements/element.h"
+
+namespace gluten
+{
+	class loading_spinner : public element
+	{
+    public:
+        loading_spinner() : element(anchor_preset::stretch_full) {}
+
+	protected:
+        auto render_element(const ImRect& elementBox) -> bool override;
+	};
+}
