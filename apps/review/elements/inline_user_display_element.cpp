ce3b1332e605fadbdc5f15982d35b05263a228fd
James Kelly <jameskelly21699@gmail.com>
2025-08-01 13:16:32 +0100

    Make votes more visual

--- apps/review/elements/inline_user_display_element.cpp
+++ apps/review/elements/inline_user_display_element.cpp
@@ -171,10 +171,10 @@ auto reviewer_display_element::render_element(const ImRect& parentRect) -> bool
     }
     else
     {
-        userNameText.set_text(fmt::format("{} - {}", m_userDisplayName,
-                                          m_vote == review_vote::no_vote  ? "No Vote"
-                                          : m_vote == review_vote::upvote ? "Upvoted"
-                                                                          : "Downvoted"));
+        userNameText.set_text(fmt::format("{}   {}", m_userDisplayName,
+                                          m_vote == review_vote::no_vote  ? ICON_LC_MINUS
+                                          : m_vote == review_vote::upvote ? ICON_LC_THUMBS_UP
+                                                                          : ICON_LC_THUMBS_DOWN));
     }
 
     userNameText.set_element_translation(ImVec2(parentRect.GetHeight() + ImGui::GetStyle().FramePadding.x, 0.0f));
