28872e4fbf11bda8f12691c20159a4f186da0bdd
James Kelly <jameskelly21699@gmail.com>
2025-08-07 17:08:02 +0100

    Render progress and handles

--- apps/review/elements/video_element.cpp
+++ apps/review/elements/video_element.cpp
@@ -7,6 +7,10 @@ namespace
     constexpr float g_videoControlsHeight = 40.0f;
     constexpr float g_videoButtonsWidth   = g_videoControlsHeight;
     constexpr float g_gapBetweenButtonsAndText = 5.0f;
+    constexpr float g_progressLineThickness    = 1.0f;
+    constexpr float g_progressLinePadding      = 5.0f;
+    constexpr float g_progressHandleWidth      = 10.0f;
+    constexpr float g_progressHandleHeight     = g_videoControlsHeight / 2.0f;
 }
 
 video_element::video_element(const std::filesystem::path& videoFile)
@@ -74,6 +78,10 @@ auto video_element::render_element(const ImRect& elementRect) -> bool
 
     const float remainingWidthMinusFinalText = elementRect.GetWidth() - (g_videoButtonsWidth * 4.0f) - g_gapBetweenButtonsAndText;
 
+    const double videoPosition = 0.0;
+    const double videoDuration = 16.0;
+    const double videoPercent  = videoPosition / videoDuration;
+
     m_videoPositionText.set_text("00:00");
     m_videoDurationText.set_text("16:00");
 
@@ -81,6 +89,27 @@ auto video_element::render_element(const ImRect& elementRect) -> bool
     m_videoControlsLayout.render_layout_element_pixels_horizontal(&m_videoPositionText, g_videoButtonsWidth);
     m_videoControlsLayout.render_layout_element_pixels_horizontal(nullptr, remainingWidthMinusFinalText);
     m_videoControlsLayout.render_layout_element_pixels_horizontal(&m_videoDurationText, g_videoButtonsWidth);
+
+    if (ImDrawList* const drawList = ImGui::GetWindowDrawList())
+    {
+        ImVec2 progressLineStart = videoControlsRect.Min;
+        progressLineStart.x += (g_videoButtonsWidth * 3.0f) + g_gapBetweenButtonsAndText + g_progressLinePadding;
+        progressLineStart.y += g_videoControlsHeight / 2.0f;
+
+        ImVec2 progressLineEnd = progressLineStart;
+        progressLineEnd.x += elementRect.GetWidth() - (g_videoButtonsWidth * 4.0f) - g_gapBetweenButtonsAndText - (g_progressLinePadding * 2.0f);
+
+        drawList->AddLine(progressLineStart, progressLineEnd, ImGui::ColorConvertFloat4ToU32(gluten::theme::carbon_g100::textPrimary), g_progressLineThickness);
+
+        const float progressX = progressLineStart.x + ((progressLineEnd.x - progressLineStart.x) * videoPercent);
+        const float progressHandleStartX = progressX - (g_progressHandleWidth / 2.0f);
+        const float progressHandleEndX   = progressHandleStartX + g_progressHandleWidth;
+        const float progressHandleStartY = progressLineStart.y - (g_progressHandleHeight / 2.0f);
+        const float progressHandleEndY   = progressHandleStartY + g_progressHandleHeight;
+
+
+        drawList->AddRectFilled(ImVec2(progressHandleStartX, progressHandleStartY), ImVec2(progressHandleEndX, progressHandleEndY), ImGui::ColorConvertFloat4ToU32(gluten::theme::carbon_g100::interactive));
+    }
 }
 
 auto video_element::get_element_content_size(const ImVec2& parentSize) -> ImVec2 const
