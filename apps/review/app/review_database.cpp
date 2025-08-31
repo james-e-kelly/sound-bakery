b8bfd732f8d124bd32e4f383803b543d97a63b7a
James Kelly <jameskelly21699@gmail.com>
2025-08-31 16:32:02 +0100

    Fix deleting reviews

--- apps/review/app/review_database.cpp
+++ apps/review/app/review_database.cpp
@@ -47,7 +47,7 @@ namespace
             phase INT DEFAULT 0,
             quality INT DEFAULT 0,
             timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-            FOREIGN KEY(project_id) REFERENCES projects(id)
+            FOREIGN KEY(project_id) REFERENCES projects(id) ON DELETE CASCADE
         );
     )sql";
 
