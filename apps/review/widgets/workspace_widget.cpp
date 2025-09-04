154ca2cde72659c15df5bf5dfc831dea160dd24a
James Kelly <jameskelly21699@gmail.com>
2025-09-04 20:33:37 +0100

    Better scoping

--- apps/review/widgets/workspace_widget.cpp
+++ apps/review/widgets/workspace_widget.cpp
@@ -520,6 +520,7 @@ void workspace_widget::render_review_content(std::shared_ptr<workspace_manager>&
 
                     if (asset.m_versionsToRelativeFiles.contains(selectedVersion))
                     {
+                        gluten::imgui::scoped_id reviewId(selectedReview.m_reviewId);
                         gluten::collapsing_header header(asset.m_fileName, false);
 
                         if (collapseAll)
