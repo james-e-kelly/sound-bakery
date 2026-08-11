dee5e8ed16ea5dd245c93d13d8918e1dc082a4a3
James Kelly <jameskelly21699@gmail.com>
2026-08-11 14:58:23 +0100

    Add custom delay DSP that can have its delay length changed by parameter

--- inc/sound_chef/sound_chef_dsp.h
+++ inc/sound_chef/sound_chef_dsp.h
@@ -94,6 +94,56 @@ extern "C"
         sc_bool isProcessing;  //< Whether start_processing() has been called on the plugin.
     } sc_clap_node;
 
+    typedef struct
+    {
+        ma_uint32 channels;
+        ma_uint32 sampleRate;
+        ma_uint32 delayInFrames;
+        ma_uint32 maxDelayInFrames;
+        float dry;      //< Dry signal gain (0 - 1). Defaults to 1
+        float wet;      //< Wet signal gain (0 - 1). Defaults to 0 (no delay)
+        float feedback; //< Feedback signal gain (0 - 1). Defaults to 0 (no feedback)
+    } sc_delay_config;
+
+    sc_delay_config SC_API sc_delay_config_init(ma_uint32 channels, ma_uint32 sampleRate, ma_uint32 maxDelayInFrames);
+
+    typedef struct
+    {
+        sc_delay_config config;
+        ma_uint32 writeCursor;
+        ma_uint32 bufferSizeInFrames;   //< Total buffer size. Not the delay time/size
+        float* buffer;
+    } sc_delay;
+
+    sbk_status  SC_API sc_delay_init(const sc_delay_config* config, const ma_allocation_callbacks* allocationCallbacks, sc_delay* delay);
+    void        SC_API sc_delay_uninit(sc_delay* delay, const ma_allocation_callbacks* allocationCallbacks);
+    sbk_status  SC_API sc_delay_process_pcm_frames(sc_delay* delay, void* framesOut, const void* framesIn, ma_uint32 frameCount);
+    sbk_status  SC_API sc_delay_set_delay_ms(sc_delay* delay, float value);
+    sbk_status  SC_API sc_delay_get_delay_ms(const sc_delay* delay, float* outValue);
+    sbk_status  SC_API sc_delay_set_wet(sc_delay* delay, float value);
+    sbk_status  SC_API sc_delay_get_wet(const sc_delay* delay, float* outValue);
+    sbk_status  SC_API sc_delay_set_dry(sc_delay* delay, float value);
+    sbk_status  SC_API sc_delay_get_dry(const sc_delay* delay, float* outValue);
+    sbk_status  SC_API sc_delay_set_feedback(sc_delay* delay, float value);
+    sbk_status  SC_API sc_delay_get_feedback(const sc_delay* delay, float* outValue);
+
+    typedef struct
+    {
+        ma_node_config nodeConfig;
+        sc_delay_config delayConfig;
+    } sc_delay_node_config;
+
+    sc_delay_node_config SC_API sc_delay_node_config_init(ma_uint32 channels, ma_uint32 sampleRate, ma_uint32 maxDelayInFrames);
+
+    typedef struct
+    {
+        ma_node_base baseNode;
+        sc_delay delay;
+    } sc_delay_node;
+
+    sbk_status  SC_API sc_delay_node_init(ma_node_graph* pNodeGraph, const sc_delay_node_config* pConfig, const ma_allocation_callbacks* pAllocationCallbacks, sc_delay_node* pDelayNode);
+    void        SC_API sc_delay_node_uninit(sc_delay_node* pDelayNode, const ma_allocation_callbacks* pAllocationCallbacks);
+
 #ifdef __cplusplus
 }
 #endif
