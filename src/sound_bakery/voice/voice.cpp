ab4c079a1faa40c313341cef309ace9e8c62013f
James Kelly <jameskelly21699@gmail.com>
2026-08-29 23:19:26 +0100

    Voices watch all properties and update at runtime

--- src/sound_bakery/voice/voice.cpp
+++ src/sound_bakery/voice/voice.cpp
@@ -11,10 +11,19 @@ using namespace sbk::engine;
 
 DEFINE_REFLECTION(voice)
 
+voice_property_watch::~voice_property_watch()
+{
+    for (auto& subscription : subscriptions)
+    {
+        subscription.delegate->Remove(subscription.handle);
+    }
+}
+
 auto sbk::engine::voice::play_container(container* container) -> sbk::result<void>
 {
     ZoneScoped;
-    remove_all();
+    m_propertyWatches.clear();
+    remove_all();   /// @todo Remove this line. Voices shouldn't have children or even derive from object
 
     gather_children_context context;
     context.sounds.push_back(container->casted_shared_from_this<sbk::engine::container>());
@@ -38,38 +47,22 @@ auto sbk::engine::voice::play_container(container* container) -> sbk::result<voi
                 continue;
             }
 
-            std::shared_ptr<node_base> outputBus = (*childIter)->get_output_bus();
-            sc_node_group* outputNodeGroup       = nullptr;
-            float volume                         = (*childIter)->m_volume.get();
-            float pitch                          = (*childIter)->m_pitch.get();
-            float lowpass                        = (*childIter)->m_lowpass.get();
-            float highpass                       = (*childIter)->m_highpass.get();
+            std::shared_ptr<node> outputBus = (*childIter)->get_output_bus();
+            sc_node_group* outputNodeGroup  = nullptr;
 
-            std::shared_ptr<node_base> parent = (*childIter)->get_parent();
+            std::shared_ptr<node> parent = (*childIter)->get_parent();
 
             while (parent)
             {
-                auto sharedParent = parent->casted_shared_from_this<node>();
-
                 // We don't have property overrides so take the bottom-most output bus
                 if (!outputBus)
                 {
-                    outputBus = sharedParent->get_output_bus();
+                    outputBus = parent->get_output_bus();
+                    break;
                 }
-
-                volume *= sharedParent->m_volume.get();
-                pitch *= sharedParent->m_pitch.get();
-
-                // For now, lowwpass and highpass is summed
-                // We can add settings later for "take highest"
-                lowpass += sharedParent->m_lowpass.get();
-                highpass += sharedParent->m_highpass.get();
-
+                
                 parent = parent->get_parent();
             }
-
-            lowpass  = std::clamp(lowpass, (*childIter)->m_lowpass.get_min(), (*childIter)->m_lowpass.get_max());
-            highpass = std::clamp(highpass, (*childIter)->m_highpass.get_min(), (*childIter)->m_highpass.get_max());
 
             if (outputBus)
             {
@@ -89,19 +82,8 @@ auto sbk::engine::voice::play_container(container* container) -> sbk::result<voi
             {
                 m_instances[index].voiceHandle = handle;
 
-                sc_voice_set_volume(runtime, handle, volume);
-                sc_voice_set_pitch(runtime, handle, pitch);
-
-                const double cutoffOffset = SC_DSP_CUTOFF_MAX - SC_DSP_CUTOFF_MIN;
-
-                const double lowpassPercentage = sbk::maths::ease_out_cubic(lowpass / 100.0);
-                const double lowpassCutoff     = (cutoffOffset - (cutoffOffset * lowpassPercentage)) + SC_DSP_CUTOFF_MIN;
-
-                const double highpassPercentage = sbk::maths::ease_in_cubic(highpass / 100.0);
-                const double highpassCutoff     = (cutoffOffset * highpassPercentage) + SC_DSP_CUTOFF_MIN;
-
-                sc_voice_set_lowpass_cutoff(runtime, handle, lowpassCutoff);
-                sc_voice_set_highpass_cutoff(runtime, handle, highpassCutoff);
+                subscribe_to_properties(handle, *childIter);
+                (void)recompute_voice_dsp(handle);
 
                 sc_voice_set_paused(runtime, handle, SC_FALSE);
             }
@@ -135,6 +117,84 @@ auto sbk::engine::voice::play_container(container* container) -> sbk::result<voi
     return sbk::ok();
 }
 
+auto voice::subscribe_to_properties(sc_voice_handle handle, const std::shared_ptr<container>& bottomContainer) -> void
+{
+    voice_property_watch watch;
+    watch.voiceHandle = handle;
+
+    auto currentNode = bottomContainer->casted_shared_from_this<node>();
+    while (currentNode)
+    {
+        auto subscribe = [&](sbk::core::float_property& prop)
+        {
+            DelegateHandle delegateHandle = prop.get_delegate().AddLambda([this, handle](float, float) { (void)recompute_voice_dsp(handle); });
+            watch.subscriptions.push_back({&prop.get_delegate(), delegateHandle});
+        };
+
+        subscribe(currentNode->m_volume);
+        subscribe(currentNode->m_pitch);
+        subscribe(currentNode->m_lowpass);
+        subscribe(currentNode->m_highpass);
+
+        watch.nodeChain.push_back(currentNode);
+        auto parent = currentNode->get_parent();
+        currentNode = parent ? parent->casted_shared_from_this<node>() : nullptr;
+    }
+
+    m_propertyWatches.push_back(std::move(watch));
+}
+
+auto voice::recompute_voice_dsp(sc_voice_handle handle) -> sbk::result<>
+{
+    runtime* const runtime = get_runtime();
+    SBK_CHECK(runtime, SBK_ERR_NULL);
+
+    const voice_property_watch* watch = nullptr;
+    for (const auto& watchIter : m_propertyWatches)
+    {
+        if (watchIter.voiceHandle == handle)
+        {
+            watch = &watchIter;
+            break;
+        }
+    }
+
+    SBK_CHECK(watch, SBK_ERR_NOT_FOUND);
+
+    float volume   = 1.0f;
+    float pitch    = 1.0f;
+    float lowpass  = 0.0f;
+    float highpass = 0.0f;
+
+    for (const auto& node : watch->nodeChain)
+    {
+        volume *= node->m_volume.get();
+        pitch *= node->m_pitch.get();
+
+        lowpass += node->m_lowpass.get();
+        highpass += node->m_highpass.get();
+    }
+
+    lowpass  = std::clamp(lowpass, 0.0f, 100.0f);
+    highpass = std::clamp(highpass, 0.0f, 100.0f);
+
+    SBK_REPORT_C(sc_voice_set_volume(runtime, handle, volume));
+    SBK_REPORT_C(sc_voice_set_pitch(runtime, handle, pitch));
+
+    const double cutoffOffset = SC_DSP_CUTOFF_MAX - SC_DSP_CUTOFF_MIN;
+
+    const double lowpassPercentage = sbk::maths::ease_out_cubic(lowpass / 100.0);
+    const double lowpassCutoff     = (cutoffOffset - (cutoffOffset * lowpassPercentage)) + SC_DSP_CUTOFF_MIN;
+
+    const double highpassPercentage = sbk::maths::ease_in_cubic(highpass / 100.0);
+    const double highpassCutoff     = (cutoffOffset * highpassPercentage) + SC_DSP_CUTOFF_MIN;
+
+    SBK_REPORT_C(sc_voice_set_lowpass_cutoff(runtime, handle, lowpassCutoff));
+    SBK_REPORT_C(sc_voice_set_highpass_cutoff(runtime, handle, highpassCutoff));
+
+    return sbk::ok();
+}
+
 auto voice::update() -> void
 {
     ZoneScoped;
@@ -147,13 +207,25 @@ auto voice::update() -> void
             {
                 unsigned int& childCount     = m_instances[index].childCount;
                 sc_voice_handle& voiceHandle = m_instances[index].voiceHandle;
-                std::size_t& parentIndex    = m_instances[index].parentIndex;
+                std::size_t& parentIndex     = m_instances[index].parentIndex;
                 sc_bool playing              = SC_FALSE;
 
                 const bool finished = m_instances[index].voiceHandle == 0 ? m_instances[index].childCount == 0 : sc_voice_get_is_playing(runtime, voiceHandle, &playing) != SBK_SUCCESS;
 
                 if (finished)
                 {
+                    if (voiceHandle != 0)
+                    {
+                        for (auto it = m_propertyWatches.begin(); it != m_propertyWatches.end(); ++it)
+                        {
+                            if (it->voiceHandle == voiceHandle)
+                            {
+                                m_propertyWatches.erase(it);
+                                break;
+                            }
+                        }
+                    }
+
                     --m_instances[parentIndex].childCount;
                     m_instances[index].finished = true;
                     m_instances[index].voiceHandle = 0;
--- src/sound_bakery/voice/voice.h
+++ src/sound_bakery/voice/voice.h
@@ -1,11 +1,37 @@
 #pragma once
 
 #include "sound_bakery/core/object/object.h"
+#include "sound_bakery/core/property.h"
 
 namespace sbk::engine
 {
     class container;
+    class node;
     class game_object;
+
+    struct container_instance
+    {
+        sbk_id containerReference{};
+        sc_voice_handle voiceHandle{};
+        unsigned int childCount{};
+        std::size_t parentIndex{};
+        bool finished{};
+    };
+
+    struct property_subscription
+    {
+        sbk::core::float_property::property_changed_delegate* delegate;
+        DelegateHandle handle;
+    };
+
+    struct voice_property_watch
+    {
+        ~voice_property_watch();
+
+        sc_voice_handle voiceHandle{};
+        eastl::vector<std::shared_ptr<node>> nodeChain;
+        eastl::vector<property_subscription> subscriptions;
+    };
 
     /**
      * @brief A runtime graph of nodes and busses, playing a sound or many.
@@ -15,22 +41,6 @@ namespace sbk::engine
         REGISTER_REFLECTION(voice, sbk::core::object)
 
     public:
-        /**
-         * @brief A voice handle -> the container that started it.
-         *
-         * When the handle ends, we can retrigger the container to see if it wants any more sounds to play.
-         */
-        using play_pair = std::pair<sc_voice_handle, std::shared_ptr<container>>;
-
-        struct container_instance
-        {
-            sbk_id containerReference{};
-            sc_voice_handle voiceHandle{};
-            unsigned int childCount{};
-            std::size_t parentIndex{};
-            bool finished{};
-        };
-
         auto play_container(container* container) -> sbk::result<void>;
 
         auto update() -> void;
@@ -43,7 +53,11 @@ namespace sbk::engine
         [[nodiscard]] auto get_output_busses() const -> const eastl::vector<std::shared_ptr<sc_node_group>>& { return m_outputBusses; }
 
     private:
+        auto subscribe_to_properties(sc_voice_handle handle, const std::shared_ptr<container>& leaf) -> void;
+        auto recompute_voice_dsp(sc_voice_handle handle) -> sbk::result<>;
+
         eastl::vector<container_instance> m_instances;
         eastl::vector<std::shared_ptr<sc_node_group>> m_outputBusses;
+        eastl::vector<voice_property_watch> m_propertyWatches;
     };
 }  // namespace sbk::engine
